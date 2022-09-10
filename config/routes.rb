32753b9599252aa9051ac03f93688d9aab12a0fa
jpierresaldana <jp.saldana@outlook.es>
2022-09-10 14:24:41 -0500

    avance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/edit', to: 'tasks#edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
